770d2911f902c614957f46dc7b83a23e74c4869f
Krzysztof <mgomc49@gmail.com>
2020-11-22 17:16:00 +0100

    update

---
+++ Assets/Shoot.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shoot : MonoBehaviour
+{
+    public Transform target;
+    public float speed = 6.0f;
+
+    void Update()
+    {
+        if (target != null)
+        {
+            float step = speed * Time.deltaTime;
+            transform.position = Vector2.MoveTowards(transform.position, target.position, step);
+        }
+    }
+}
